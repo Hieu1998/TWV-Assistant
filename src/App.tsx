a672117f8196161a5ea86fa8eaf6aae7c205c351
Hieu1998 <41219963+Hieu1998@users.noreply.github.com>
2026-03-24 14:45:55 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import PostGenerator from './pages/PostGenerator';
@@ -14,7 +14,7 @@ import Appointments from './pages/Appointments';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
@@ -25,6 +25,6 @@ export default function App() {
           <Route path="calendar" element={<ContentCalendar />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
